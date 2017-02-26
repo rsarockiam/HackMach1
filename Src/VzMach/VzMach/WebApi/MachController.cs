44224d95ca4c0e213c9f8e162babdb9fe62ceeae
v921273 <sundaravadivel.x.chandran@one.verizon.com>
2017-02-26 17:36:17 +0530

    Logic adjusted for bundles

--- Src/VzMach/VzMach/WebApi/MachController.cs
+++ Src/VzMach/VzMach/WebApi/MachController.cs
@@ -16,7 +16,7 @@ namespace VzMach.WebApi
     public class MachController : ApiController
     {
         DataSet Data { get { return ExcelHelper.ExcelData; } }
-        const string  zipPopular= "ZIPPOPULAR"; const string countryPopular = "COUNTRYPOPULAR"; const string subbndlValue = "SubBundleValue";
+        const string zipPopular = "ZIPPOPULAR"; const string countryPopular = "COUNTRYPOPULAR"; const string subbndlValue = "SubBundleValue";
         #region LocationDetails
         [Route("~/WebApi/GetLocaionDetails")]
         [HttpGet]
@@ -119,21 +119,44 @@ namespace VzMach.WebApi
             return Json(JsonConvert.SerializeObject(recModel));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type">CORE/COMP</param>
+        /// <returns></returns>
 
-        #region GetBundles
-        [Route("~/WebApi/GetBundles")]
+        #region GetAllBundles
+        [Route("~/WebApi/GetAllBundles")]
         [HttpGet]
         #endregion
-        public IHttpActionResult GetBundles(string type = "")
+        
+        public IHttpActionResult GetAllBundlesByType(string type="")
         {
 
-            dynamic GetBundles = null;
-            return GetBundles;
+            List<BundleModel> Bundles = new List<BundleModel>();
+            foreach (DataRow row in Data.Tables[1].Rows)
+            {      
+                if (row != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(type) && row["Type"].ToString().Trim() != type)
+                        continue;
+                    BundleModel bun = new BundleModel();
+                    bun.BundleId = row["BundleId"].ToString();
+                    bun.Type = row["Type"].ToString();
+                    bun.Name = row["Name"].ToString();
+                    bun.Price = row["Price"].ToString();
+                    bun.DAT = row["DAT"].ToString();
+                    bun.TV = row["TV"].ToString();
+                    bun.VOICE = row["VOICE"].ToString();
+                    bun.ROUTER = row["ROUTER"].ToString();
+                    bun.Discount = row["Discount"].ToString();
+                    bun.Keyword = row["Keyword"].ToString();
+                    Bundles.Add(bun);
+                }
+            }
+
+            return Json(JsonConvert.SerializeObject(Bundles));
         }
-
-
-
-
 
     }
 }

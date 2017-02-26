251302d21cbb942fb9c8853cbef5689ca449d58c
Administrator <Administrator@WIN-BO8QR0HI2VJ>
2017-02-26 09:13:34 +0000

    Location details

--- Src/VzMach/VzMach/WebApi/MachController.cs
+++ Src/VzMach/VzMach/WebApi/MachController.cs
@@ -7,22 +7,35 @@ using System.Web.Http;
 using Newtonsoft.Json;
 using System.Dynamic;
 using VzMach.Helper;
+using System.IO;
 
 namespace VzMach.WebApi
 {
     public class MachController : ApiController
     {
-       
+
         #region LocationDetails
         [Route("~/WebApi/GetLocaionDetails")]
         [HttpGet]
-        #endregion
-        public IHttpActionResult GetLocaionDetails(string type="")
+        public IHttpActionResult GetLocaionDetails(string type = "")
         {
-            var x = ExcelHelper.ExcelData;
-            dynamic location = null;
-            return location;
+            const string DATA = @"{""object"":{""name"":""Name""}}";
+            string response = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://ip-api.com/json/" + type);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = DATA.Length;
+            StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+            requestWriter.Write(DATA);
+            requestWriter.Close();
+            WebResponse webResponse = request.GetResponse();
+            Stream webStream = webResponse.GetResponseStream();
+            StreamReader responseReader = new StreamReader(webStream);
+            response = responseReader.ReadToEnd();
+            responseReader.Close();
+            return Json(response);
         }
+        #endregion
 
         #region RecommendPlans
         [Route("~/WebApi/GetRecommendPlans")]
